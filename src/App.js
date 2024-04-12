e9443533265b46f3c62612d531f9cb3424a08803
oussama <ohamzeh10@gmail.com>
2024-04-13 00:06:01 +0300

    oussama

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>oussama hamzeh </h1>
+      <div className="gridcontainer">
+        <div style={{ backgroundColor: 'red' }}>Row 1, Column 1</div>
+        <div style={{ backgroundColor: 'blue' }}>Row 1, Column 2</div>
+        <div style={{ backgroundColor: 'green' }}>Row 2, Column 1</div>
+        <div style={{ backgroundColor: 'yellow' }}>Row 2, Column 2</div>
+      </div>
+      <div className="gridcontainer1">
+        <div style={{ backgroundColor: 'purple' }}>Item 1</div>
+        <div style={{ backgroundColor: 'orange' }}>Item 2</div>
+        <div style={{ backgroundColor: 'pink' }}>Item 3</div>
+      </div>
     </div>
   );
 }
